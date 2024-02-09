0f558f9a45ab8d6ca350346b7e8ed113a10d0342
Mirkosz <mirkoszevajczuk@gmail.com>
2024-02-08 21:16:26 -0300

    first commit
    full proyect

---
+++ script.js
@@ -0,0 +1,44 @@
+
+let playBoton = document.getElementById("play");
+let pauseBoton= document.getElementById("pause");
+let video=document.querySelector('video');
+
+setTimeout(()=>{
+    document.getElementsByClassName('tiempo').innerHTML=` Duracion video  4:41`},100);
+    for (let i = 0; i < video.time; i++) {
+    console.log(video.currentTime)
+};
+
+playBoton.addEventListener('click',()=>{
+    video.play()
+});
+
+pauseBoton.addEventListener('click',()=>{
+    video.pause();
+    flag=false;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
