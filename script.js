a84875697eaac184639a7d2c452c299281903c30
Mirkosz <mirkoszevajczuk@gmail.com>
2024-02-08 22:25:08 -0300

    fix details

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let playBoton = document.getElementById("play");
 let pauseBoton= document.getElementById("pause");
 let video=document.querySelector('video');
@@ -6,39 +5,12 @@ let video=document.querySelector('video');
 setTimeout(()=>{
     document.getElementsByClassName('tiempo').innerHTML=` Duracion video  4:41`},100);
     for (let i = 0; i < video.time; i++) {
-    console.log(video.currentTime)
+    console.log(video.currentTime);
 };
-
 playBoton.addEventListener('click',()=>{
-    video.play()
+    video.play();
 });
-
 pauseBoton.addEventListener('click',()=>{
     video.pause();
     flag=false;
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- script2.js
+++ script2.js
@@ -1,67 +1,59 @@
-let img1 = document.getElementById("imgrompe1")
-let drop1= document.getElementById("cuadrodrop1")
-let img2 = document.getElementById("imgrompe2")
-let drop2= document.getElementById("cuadrodrop2")
-let img3 = document.getElementById("imgrompe3")
-let drop3= document.getElementById("cuadrodrop3")
+let img1 = document.getElementById("imgrompe1");
+let drop1= document.getElementById("cuadrodrop1");
+let img2 = document.getElementById("imgrompe2");
+let drop2= document.getElementById("cuadrodrop2");
+let img3 = document.getElementById("imgrompe3");
+let drop3= document.getElementById("cuadrodrop3");
 
 
 img1.addEventListener("dragstart",(e)=>{
-    e.dataTransfer.setData(`Text`,"./assets/imagenes/Rompe1.png")  
+    e.dataTransfer.setData(`Text`,"./assets/imagenes/Rompe1.png");  
 });
 img1.addEventListener("dragend",(e)=>{
     let imagen= e.target
     imagen.style.display="none"
-})
+});
 drop1.addEventListener("dragover",(e)=>{
-    e.preventDefault()
-} )
+    e.preventDefault();
+});
 drop1.addEventListener("drop", (e)=>{
-    let infoimg1 = e.dataTransfer.getData(`Text`)
-    drop1.innerHTML = `<img id="imgrompe1"  src="${infoimg1}"/>`  
-})
+    let infoimg1 = e.dataTransfer.getData(`Text`);
+    drop1.innerHTML = `<img id="imgrompe1"  src="${infoimg1}"/>`;  
+});
 
 
 img2.addEventListener("dragstart",(e)=>{
-    e.dataTransfer.setData(`Text`,"./assets/imagenes/rompe2.png")
+    e.dataTransfer.setData(`Text`,"./assets/imagenes/rompe2.png");
 });
 img2.addEventListener("dragend",(e)=>{
     let imagen= e.target
     imagen.style.display="none"
-})
+});
 drop2.addEventListener("dragover",(e)=>{
-    e.preventDefault()
-})
+    e.preventDefault();
+});
 drop2.addEventListener("drop", (e)=>{
-    let infoimg2 = e.dataTransfer.getData(`Text`)
-    drop2.innerHTML = `<img id="imgrompe2"  src="${infoimg2}"/>`
-})
+    let infoimg2 = e.dataTransfer.getData(`Text`);
+    drop2.innerHTML = `<img id="imgrompe2"  src="${infoimg2}"/>`;
+});
 
 
 img3.addEventListener("dragstart",(e)=>{
-    e.dataTransfer.setData(`Text`,"./assets/imagenes/Rompe3.png")
+    e.dataTransfer.setData(`Text`,"./assets/imagenes/Rompe3.png");
 });
 img3.addEventListener("dragend",(e)=>{
     let imagen= e.target
     imagen.style.display="none"
-})
+});
 drop3.addEventListener("dragover",(e)=>{
-    e.preventDefault()
-})
+    e.preventDefault();
+});
 drop3.addEventListener("drop", (e)=>{
-    let infoimg3 = e.dataTransfer.getData(`Text`)
-    drop3.innerHTML = `<img id="imgrompe3"  src="${infoimg3}"/>`
-})
+    let infoimg3 = e.dataTransfer.getData(`Text`);
+    drop3.innerHTML = `<img id="imgrompe3"  src="${infoimg3}"/>`;
+});
 
 
 function reiniciar() {
     window.location.reload();
-}
-
-
-
-if(img1 === drop1){
-    prompt("pacooooo")
-}
-
-
+};
